16608888c9d57fd1a7580c14fcc5e2e8d8899dfb
zwehtetaungjp-ui <zwehtetaung.jp@gmail.com>
2026-02-18 15:26:48 +0900

    Update diamondbot.py

--- diamondbot.py
+++ diamondbot.py
@@ -45,7 +45,7 @@ st.set_page_config(page_title="MLBB Shop", page_icon="💎", layout="centered")
 st.markdown("""
     <style>
     div.stButton > button {
-        width: 100%;
+        width: 80%;
         height: 150px;
         border-radius: 15px;
         border: 1px solid #ddd;
@@ -134,3 +134,4 @@ if st.button(t["btn"], use_container_width=True, type="primary"):
                 st.error("Telegram Error! Check Token/ID.")
     else:
         st.error(t["error"])
+
