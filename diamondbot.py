5750eed98af51d461ae7f1fde300a79e5371a816
zwehtetaungjp-ui <zwehtetaung.jp@gmail.com>
2026-02-18 23:17:03 +0900

    Update diamondbot.py

--- diamondbot.py
+++ diamondbot.py
@@ -102,7 +102,7 @@ if st.session_state.selected_pack:
 st.markdown("---")
 with st.container(border=True):
     st.markdown(f"*Transfer to {currency} Address:*")
-    if currency == "MMK": st.code("KPay: 09256084562(U ZWE HTET AUNG")
+    if currency == "MMK": st.code("KPay: 09256084562(U ZWE HTET AUNG)")
     elif currency == "JPY": st.code("PayPay Money : 08042419779")
     else: st.code("USDT (TRC20): TXXXXXXXXXXXXXXXX")
 
@@ -134,6 +134,7 @@ if st.button(t["btn"], use_container_width=True, type="primary"):
                 st.error("Telegram Error! Check Token/ID.")
     else:
         st.error(t["error"])
+
 
 
 
